f6ec04b02a5de7495b4a9e627cc47b7f3d75cbef
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-06-19 14:46:32 +0000

    Receive repository as a command-line argument
    Use `structopt` to parse the command-line arguments. Expect an owner
    parameter and a repository parameter.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use structopt::StructOpt;
+
+/// Command-line arguments.
+#[derive(Clone, Debug, StructOpt)]
+pub struct Arguments {
+    /// The GitHub user or organization that owns the repository.
+    #[structopt(short, long)]
+    owner: String,
+
+    /// The GitHub repository.
+    #[structopt(short, long)]
+    repo: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let arguments = Arguments::from_args();
+
+    dbg!(arguments);
 }
