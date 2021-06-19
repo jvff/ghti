7d3a7aa5b6dee59c9bb6fa25362175ef2c2bd1c3
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2021-06-19 15:08:59 +0000

    Allow using a `GITHUB_REPO` environment variable
    To avoid having to write command line paramaters repeatedly.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,46 @@
-use structopt::StructOpt;
+use {std::env, structopt::StructOpt};
 
 /// Command-line arguments.
 #[derive(Clone, Debug, StructOpt)]
 pub struct Arguments {
     /// The GitHub user or organization that owns the repository.
-    #[structopt(short, long)]
-    owner: String,
+    #[structopt(short, long, requires("repo"))]
+    owner: Option<String>,
 
     /// The GitHub repository.
-    #[structopt(short, long)]
-    repo: String,
+    #[structopt(short, long, requires("owner"))]
+    repo: Option<String>,
 }
 
 fn main() {
     let arguments = Arguments::from_args();
+    let owner;
+    let repo;
 
-    dbg!(arguments);
+    if let (Some(owner_argument), Some(repo_argument)) = (arguments.owner, arguments.repo) {
+        owner = owner_argument;
+        repo = repo_argument;
+    } else {
+        // Since the repo parameter requires the owner parameter, if owner is not set then repo is
+        // also not set
+        let owner_and_repo = env::var("GITHUB_REPO").expect("Missing GitHub repository");
+        let mut parts = owner_and_repo.split("/");
+
+        owner = parts
+            .next()
+            .expect("Empty GITHUB_REPO environment variable")
+            .to_owned();
+        repo = parts
+            .next()
+            .expect("Incorrect GITHUB_REPO environment variable, must be <owner>/<repo>")
+            .to_owned();
+
+        assert!(
+            parts.next().is_none(),
+            "Incorrect GITHUB_REPO environment variable, must be <owner>/<repo>"
+        );
+    }
+
+    dbg!(owner);
+    dbg!(repo);
 }
